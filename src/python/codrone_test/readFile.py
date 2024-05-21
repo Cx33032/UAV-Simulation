fa3bdec23e033cee94a44c8c2615ef99d972def3
ChikinWeirdo <chikinplayz@gmail.com>
2024-05-20 21:50:22 -0400

    Update readFile.py
    - added a list for distances and directions
    - set directions and distances to variables to hold within the lists
    - cut down a bit of the code with the help of Harry

--- src/python/codrone_test/readFile.py
+++ src/python/codrone_test/readFile.py
@@ -1,20 +1,21 @@
-'''
-file = open(r"src\python\codrone_test\testFile.txt", "r")
+file = open("src/python/codrone_test/testFile.txt")
 fileContent = file.readlines() #reads the contents of the file, every line
-lineCount = len(fileContent) #how many lines are in the file
 
-for i in range(lineCount):
-    line = fileContent[i] 
-    index = line.find(":") #finds the index of :
-    if (index != -1):
-        if i%2 == 0: 
-            print("Direction is: " + line[index+2:], end="") #prints everything after : for direction
-        else:
-            print("Moves: " + line[index+2:], end="") #prints everything after : for how far it moves
-    
+directions = []
+distances = []
+
+for line in fileContent:
+    if line.find("Direction: ") != -1:
+        directions.append(line[11:].strip())
+    else:
+        distances.append(line[6:].strip())
+
+for i in range(len(directions)):
+    print(directions[i] + ": " + distances[i])
+
 file.close() #closes the file after reading 
-'''
 
+'''
 file = open('src/python/codrone_test/testFile.txt')
 lines = file.readlines()
 
@@ -25,3 +26,4 @@ for line in lines:
         print(f'The distance is {line.replace('Move: ', '')}')
 
 file.close()
+'''
