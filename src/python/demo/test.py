a338ca6a6b1962fa128fb50fa411a3027671ed58
Cx330 <3228838462@qq.com>
2024-05-22 21:10:45 -0400

    Add more functions

--- src/python/demo/test.py
+++ src/python/demo/test.py
@@ -1,10 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from codrone_edu.drone import *
+import time
 
 app = Flask(__name__)
+# drone = Drone()
+# drone.pair()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('./index.html')
+
+@app.route('/takeoff/', methods=['GET', 'POST'])
+def takeoff():
+    print('takeoff')
+    # drone.takeoff()
+    time.sleep(1)
+    return redirect('/')
+
+@app.route('/land/', method=['GET', 'POST'])
+def land():
+    print('land')
+    # drone.land()
+    # drone.close()
+    exit(0)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
