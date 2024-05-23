70ca8c887e6261dc11af3e21cc5b05ddcc41ebc2
Yiming <68908279+Cx33032@users.noreply.github.com>
2024-05-23 10:01:59 -0400

    Update test.py

--- src/python/demo/test.py
+++ src/python/demo/test.py
@@ -10,20 +10,52 @@ app = Flask(__name__)
 def home():
     return render_template('./index.html')
 
+@app.route('/air/', methods=['GET', 'POST'])
+def air():
+    return render_template('takeoff.html')
+
 @app.route('/takeoff/', methods=['GET', 'POST'])
 def takeoff():
     print('takeoff')
     # drone.takeoff()
     time.sleep(1)
-    return redirect('/')
+    return redirect('/air/')
 
 @app.route('/land/', methods=['GET', 'POST'])
 def land():
     print('land')
     # drone.land()
     # drone.close()
-    exit(0)
+    time.sleep(1)
+    return redirect('/')
 
+@app.route('/forward/', methods=['GET', 'POST'])
+def forward():
+    print('forward')
+    # drone.sendControlWhile(0, 100, 0, 0, 500)
+    time.sleep(1)
+    return redirect('/air/')
+
+@app.route('/backward/', methods=['GET', 'POST'])
+def backward():
+    print('backward')
+    # drone.sendControlWhile(0, -100, 0, 0, 500)
+    time.sleep(1)
+    return redirect('/air/')
+
+@app.route('/left/', methods=['GET', 'POST'])
+def left():
+    print('left')
+    # drone.sendControlWhile(-100, 0, 0, 0, 500)
+    time.sleep(1)
+    return redirect('/air/')
+
+@app.route('/right/', methods=['GET', 'POST'])
+def right():
+    print('right')
+    # drone.sendControlWhile(100, 0, 0, 0, 500)
+    time.sleep(1)
+    return redirect('/air/')
 
 
 if __name__ == '__main__':
