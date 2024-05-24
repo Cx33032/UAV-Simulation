5bc37a8143679008f07ba7585a8a3e8254e42bfb
Yiming <68908279+Cx33032@users.noreply.github.com>
2024-05-24 11:20:51 -0400

    Update test.py

--- src/python/demo/test.py
+++ src/python/demo/test.py
@@ -3,8 +3,8 @@ from codrone_edu.drone import *
 import time
 
 app = Flask(__name__)
-# drone = Drone()
-# drone.pair()
+drone = Drone()
+# 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -12,48 +12,50 @@ def home():
 
 @app.route('/air/', methods=['GET', 'POST'])
 def air():
+    drone.reset_move()
     return render_template('takeoff.html')
 
 @app.route('/takeoff/', methods=['GET', 'POST'])
 def takeoff():
     print('takeoff')
-    # drone.takeoff()
+    drone.pair()
+    drone.takeoff()
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/land/', methods=['GET', 'POST'])
 def land():
     print('land')
-    # drone.land()
-    # drone.close()
+    drone.land()
+    drone.close()
     time.sleep(1)
     return redirect('/')
 
 @app.route('/forward/', methods=['GET', 'POST'])
 def forward():
     print('forward')
-    # drone.sendControlWhile(0, 100, 0, 0, 500)
+    drone.sendControlWhile(0, 100, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/backward/', methods=['GET', 'POST'])
 def backward():
     print('backward')
-    # drone.sendControlWhile(0, -100, 0, 0, 500)
+    drone.sendControlWhile(0, -100, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/left/', methods=['GET', 'POST'])
 def left():
     print('left')
-    # drone.sendControlWhile(-100, 0, 0, 0, 500)
+    drone.sendControlWhile(-100, 0, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/right/', methods=['GET', 'POST'])
 def right():
     print('right')
-    # drone.sendControlWhile(100, 0, 0, 0, 500)
+    drone.sendControlWhile(100, 0, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
@@ -68,6 +70,8 @@ def spiral():
             drone.sendControlWhile(0, 100, 0, 0, i * 500)
             drone.sendControlWhile(100, 0, 0, 0, i * 500)
             print(drone.get_position_data)
+    
+    drone.reset_move()
     return redirect('/air/')
     
 
