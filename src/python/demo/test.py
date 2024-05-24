7a2ad07c19d3998f3e2ac9fc34ac69742603772e
Shapeshifter24 <akash17844@gmail.com>
2024-05-24 10:49:09 -0400

    update

--- src/python/demo/test.py
+++ src/python/demo/test.py
@@ -3,7 +3,7 @@ from codrone_edu.drone import *
 import time
 
 app = Flask(__name__)
-# drone = Drone()
+drone = Drone()
 # drone.pair()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,43 +17,44 @@ def air():
 @app.route('/takeoff/', methods=['GET', 'POST'])
 def takeoff():
     print('takeoff')
-    # drone.takeoff()
+    drone.pair()
+    drone.takeoff()
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/land/', methods=['GET', 'POST'])
 def land():
     print('land')
-    # drone.land()
-    # drone.close()
+    drone.land()
+    drone.close()
     time.sleep(1)
     return redirect('/')
 
 @app.route('/forward/', methods=['GET', 'POST'])
 def forward():
     print('forward')
-    # drone.sendControlWhile(0, 100, 0, 0, 500)
+    drone.sendControlWhile(0, 100, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/backward/', methods=['GET', 'POST'])
 def backward():
     print('backward')
-    # drone.sendControlWhile(0, -100, 0, 0, 500)
+    drone.sendControlWhile(0, -100, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/left/', methods=['GET', 'POST'])
 def left():
     print('left')
-    # drone.sendControlWhile(-100, 0, 0, 0, 500)
+    drone.sendControlWhile(-100, 0, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
 @app.route('/right/', methods=['GET', 'POST'])
 def right():
     print('right')
-    # drone.sendControlWhile(100, 0, 0, 0, 500)
+    drone.sendControlWhile(100, 0, 0, 0, 500)
     time.sleep(1)
     return redirect('/air/')
 
